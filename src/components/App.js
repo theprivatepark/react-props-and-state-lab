a6587c2b6d346a95f97bcf3b8014b7641d1cb9f0
theprivatepark <anneypark@gmail.com>
2020-10-29 01:20:25 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,68 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
+  // constructor() {
+  //   super()
 
-    this.state = {
-      pets: [],
-      filters: {
-        type: 'all'
-      }
+  //   this.state = {
+  //     pets: [],
+  //     filters: {
+  //       type: 'all'
+  //     }
+  //   }
+  // }
+
+  // cool kid syntax
+  state = {
+    pets: [],
+    filters: {
+      type: 'all'
+      
     }
   }
+
+  onChangeType = (event) => {
+    console.log(event)
+    this.setState({
+      filters : {
+        type: event
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let url = '/api/pets'
+    if (this.state.filters.type == 'all')
+    fetch(url)
+    .then(response => response.json())
+    .then(allPetsArray => this.setState({
+      pets: allPetsArray
+    }))
+    else {
+      fetch(url + `?type=${this.state.filters.type}`)
+      .then(response => response.json())
+      .then(onePetsArray => this.setState({
+        pets: onePetsArray
+      }))
+    }
+  }
+
+onAdoptPet = (id) => {
+  let newPetArray = [...this.state.pets.map(pet => {
+    if (pet.id == id){
+      return {...pet,
+        isAdopted: true 
+      }
+    }
+    return pet //explicit return is undefined
+  })]
+  // console.log(newPetArray)
+  // debugger
+  this.setState({
+    pets: newPetArray
+  })
+
+}
 
   render() {
     return (
@@ -24,10 +76,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
